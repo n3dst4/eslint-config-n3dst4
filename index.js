4bdb687961b61e5292c69f967319b29f68b48bc9
Neil de Carteret <git@lumphammer.com>
2016-06-16 15:03:52 +0100

    relax some babel rules

--- index.js
+++ index.js
@@ -49,11 +49,12 @@ module.exports = {
     // replace certain posibly-problematic eslint rules with reimplemented ones
     // from eslint-plugin-babel
     "babel/generator-star-spacing": 1,
-    "babel/new-cap": 1,
-    "babel/object-curly-spacing": 1,
+    "babel/new-cap": 0,
+    "babel/object-curly-spacing": 0,
     "babel/object-shorthand": 1,
-    "babel/arrow-parens": 1,
+    "babel/arrow-parens": 0,
     "babel/no-await-in-loop": 1,
+
 
     // disable the corresponding ones from eslint core
     "generator-star-spacing": 0,
